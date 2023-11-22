cfd766a13de2a3dbab04e2228c212b7be5f90ba7
Alexey <chiglincev2010@bk.ru>
2023-11-22 16:58:10 +0500

    Сhange import in brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 /* eslint-disable import/extensions */
-import name from '../src/cli.js';
-import gameParity from '../src/games/even.js';
+import name from '../src/index.js';
+import gameEven from '../src/games/even.js';
 
 console.log(`Hello, ${name}!`);
 console.log(`${gameEven(3)}${name}!`);
