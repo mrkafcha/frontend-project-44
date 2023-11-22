0d2757e2c6b49538e8080157625676c66ff4527b
Alexey <chiglincev2010@bk.ru>
2023-11-22 14:25:17 +0500

    Fix content even.js

--- src/even.js
+++ src/even.js
@@ -4,26 +4,34 @@ import random from 'lodash/random.js';
 import readlineSync from 'readline-sync';
 
 const gameParity = () => {
-  for (let i = 1; i <= 3; i += 1) {
+  let i = 1;
+  while (i <= 3) {
     const num = random(1, 50);
     const result = num % 2;
     console.log(`Question: ${num}`);
     const answer = readlineSync.question('Your answer: ');
 
-    if (result === 0) {
-      if (answer === 'yes') {
-        console.log('Correct!');
-      } else {
+    switch (answer) {
+      case 'yes':
+        if (result === 0) {
+          console.log('Correct!');
+        } else {
+          return "'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ";
+        }
+        break;
+      case 'no':
+        if (result !== 0) {
+          console.log('Correct!');
+        } else {
+          return "'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ";
+        }
+        break;
+      default:
         return `'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, `;
-      }
-    } else if (result !== 0) {
-      if (answer === 'no') {
-        console.log('Correct!');
-      } else {
-        return `'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, `;
-      }
     }
+    i += 1;
   }
+
   return 'Congratulations, ';
 };
 
