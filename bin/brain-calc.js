f4bf032206dbf0d3cabf791ec30ed93f46af763b
Alexey <chiglincev2010@bk.ru>
2023-11-22 16:44:40 +0500

    Fix content brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -2,8 +2,7 @@
 /* eslint-disable no-console */
 /* eslint-disable import/extensions */
 import name from '../src/cli.js';
-import gameCalc from '../src/calc.js';
+import gameCalc from '../src/games/calc.js';
 
 console.log(`Hello, ${name}!`);
-console.log('What is the result of the expression?');
 console.log(`${gameCalc(3)}${name}!`);
