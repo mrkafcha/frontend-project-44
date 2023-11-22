aae10f1d3fcbe80a19b2a8bd6d7dd1145f9e5ca8
Alexey <chiglincev2010@bk.ru>
2023-11-22 11:57:32 +0500

    Add game brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+import name from '../src/cli.js';
+import gameCalc from '../src/calc.js';
+
+
+console.log(`Hello, ${name}!`);
+console.log("What is the result of the expression?");
+console.log(`${gameCalc()}${name}!`);
---
+++ src/calc.js
@@ -0,0 +1,44 @@
+
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+import random from 'lodash/random.js';
+import readlineSync from 'readline-sync';
+
+const randomOperators = () => {
+    const operation = ['+' , '-' , '*'];
+    const randomIndex = random(0, 2); 
+    return operation[randomIndex]; 
+}
+
+
+const gameCalc = () => {
+  for (let i = 1; i <= 3; i += 1) {
+    const numOne = random(1, 20);
+    const numTwo = random(1, 20);
+    let result = 0;
+    const operation = randomOperators();
+    console.log(`Question: ${numOne} ${operation} ${numTwo}`);
+    const answer = Number(readlineSync.question('Your answer: '));
+    
+    switch (operation) {
+      case '-':
+        result = numOne - numTwo;
+        break;
+      case '+':
+        result = numOne + numTwo;
+        break;
+      case '*':
+        result = numOne * numTwo;
+    }
+
+    if (answer === result) {
+      console.log('Correct!');
+    } else {
+      return `'${answer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, `;
+    }
+  }
+
+  return 'Congratulations, ';
+};
+
+export default gameCalc;
