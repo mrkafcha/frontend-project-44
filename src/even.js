1f9a0d19b166c12fef29fbb1e1f5c8ff641bb69a
Alexey <chiglincev2010@bk.ru>
2023-11-22 14:12:10 +0500

    Fix conten even.js

--- src/even.js
+++ src/even.js
@@ -6,30 +6,24 @@ import readlineSync from 'readline-sync';
 const gameParity = () => {
   for (let i = 1; i <= 3; i += 1) {
     const num = random(1, 50);
-    const parity = num % 2;
+    const result = num % 2;
     console.log(`Question: ${num}`);
     const answer = readlineSync.question('Your answer: ');
 
-    switch (answer) {
-      case 'yes':
-        if (parity === 0) {
-          console.log('Correct!');
-        } else {
-          return "'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ";
-        }
-        break;
-      case 'no':
-        if (parity !== 0) {
-          console.log('Correct!');
-        } else {
-          return "'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ";
-        }
-        break;
-      default:
+    if (result === 0) {
+      if (answer === 'yes') {
+        console.log('Correct!');
+      } else {
         return `'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, `;
+      }
+    } else if (result !== 0) {
+      if (answer === 'no') {
+        console.log('Correct!');
+      } else {
+        return `'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, `;
+      }
     }
   }
-
   return 'Congratulations, ';
 };
 
