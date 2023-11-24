f5ab9137e34a2d476dc8f556fdfa087e6f30ebc4
Alexey <chiglincev2010@bk.ru>
2023-11-24 13:22:25 +0500

    Fix content index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
+/* eslint-disable no-console */
 import readlineSync from 'readline-sync';
 
-const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+export default (gameLogic) => {
+  const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  const initialDescription = gameLogic();
+  console.log(initialDescription[0]);
 
-export default name;
+  for (let i = 1; i <= 3; i += 1) {
+    const gameGenerator = gameLogic();
+    const question = gameGenerator[1];
+    const result = gameGenerator[2];
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (String(result) === answer) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, `);
+    }
+  }
+
+  return console.log(`Congratulations, ${userName}`);
+};
