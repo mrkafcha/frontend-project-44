8f3181f3f48477e28345b3275e3ec09a0b8a8831
Alexey <chiglincev2010@bk.ru>
2023-11-24 23:29:00 +0500

    Fix game content brain-progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-/* eslint-disable no-console */
-/* eslint-disable import/extensions */
 import name from '../src/index.js';
-import gameProgression from '../src/games/progression.js';
+import runGameProgression from '../src/games/progression.js';
 
-name(gameProgression);
+name(runGameProgression);
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,35 +1,34 @@
-/* eslint-disable no-console */
-/* eslint-disable import/extensions */
-import random from 'lodash/random.js';
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-const strNumbers = (start, length, hidden, order) => {
-  const arrNumbers = [];
+const getArrProgression = (start, length, interval) => {
+  const arrProgression = [];
   let numStart = start;
 
   for (let i = 0; i <= length; i += 1) {
-    if (i === hidden) {
-      arrNumbers.push('..');
-      numStart += order;
-    } else {
-      arrNumbers.push(numStart);
-      numStart += order;
-    }
+    arrProgression.push(numStart);
+    numStart += interval;
   }
 
-  return arrNumbers;
+  return arrProgression;
 };
 
-const gameProgression = () => {
+const runGameProgression = () => {
+  const startInterval = 1;
+  const endInterval = 41;
+  const endLengthInterval = 11;
+  const startLengthProgression = 4;
+  const endLengthProgression = 10;
   const startDescription = 'What number is missing in the progression?';
-  const numstart = random(1, 40);
-  const lengthNum = random(4, 9);
-  const hiddenNum = random(1, lengthNum);
-  const orderNum = random(2, 7);
-  const arrProgression = strNumbers(numstart, lengthNum, hiddenNum, orderNum);
+  const numstart = getRandomInt(startInterval, endInterval);
+  const lengthProgression = getRandomInt(startLengthProgression, endLengthProgression);
+  const hiddenNum = getRandomInt(startInterval, lengthProgression);
+  const lengthInterval = getRandomInt(startInterval, endLengthInterval);
+  const arrProgression = getArrProgression(numstart, lengthProgression, lengthInterval);
+  const result = String(arrProgression[hiddenNum]);
+  arrProgression[hiddenNum] = '..';
   const question = `${arrProgression.join(' ')}`;
-  const result = arrProgression[hiddenNum - 1] + orderNum;
 
   return [startDescription, question, result];
 };
 
-export default gameProgression;
+export default runGameProgression;
