0c683bfcdf392b82b4053ea4eda2c2e58c6e7307
Alexey <chiglincev2010@bk.ru>
2023-11-27 12:01:04 +0500

    Fix predicate prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,11 +6,11 @@ const getEvenResult = (num) => {
   let i = Math.round(num / 2);
   for (i; i >= 2; i -= 1) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
   }
 
-  return 'yes';
+  return true;
 };
 
 const runGamePrime = () => {
@@ -18,7 +18,7 @@ const runGamePrime = () => {
   const endInterval = 53;
   const startDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   const num = getRandomInt(startInterval, endInterval);
-  const result = String(getEvenResult(num));
+  const result = getEvenResult(num) ? 'yes' : 'no';
   const question = `${num}`;
 
   return [startDescription, question, result];
