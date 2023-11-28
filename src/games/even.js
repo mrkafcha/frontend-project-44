daed58dc27d80808cb13da3f058c719e006efcd5
Alexey <chiglincev2010@bk.ru>
2023-11-28 11:51:24 +0500

    Fix content even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,16 +1,15 @@
 import runGameLogic from '../index.js';
 import getRandomInt from '../random.js';
 
-const checkEquality = (num) => (num === 0);
+const isEven = (num) => (num % 2 === 0);
 
 const startDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+const startInterval = 1;
+const endInterval = 51;
 
 const runGameEven = () => {
-  const startInterval = 1;
-  const endInterval = 51;
   const num = getRandomInt(startInterval, endInterval);
-  const even = Math.sign(num % 2);
-  const result = checkEquality(even) ? 'yes' : 'no';
+  const result = isEven(num) ? 'yes' : 'no';
 
   return [String(num), result];
 };
